cf5c2bcac6aee3d9ea4338ab5b8de980aa13cfc8
Tejas Sharma <s.tejas.ak@gmail.com>
2024-06-13 11:26:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+
+    app = Flask(__name__)
+
+    # client = MongoClient("mongodb+srv://tejas:Pm(mp)05@microblog-application.h8gjluh.mongodb.net/?retryWrites=true&w=majority&appName=Microblog-Application&tlsAllowInvalidCertificates=true")
+
+    client = MongoClient(os.getenv("MONGODB_URI"))
+
+    app.db = client.microblog
+
+    # Ensure the 'entries' collection exists in MongoDB
+    app.db.entries = app.db.get_collection('entries')
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            # If the request method is POST, then we can grab the content and do something with it.
+            # "content" is the name of the text field.
+            entry_content = request.form.get("content")
+            # We also need the date when the entry was created.
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        # Retrieve entries from MongoDB and format dates
+        entries_with_date = []
+        for entry in app.db.entries.find({}):
+            content = entry.get("content", "No content")
+            date_str = entry.get("date", "")
+            if date_str:
+                try:
+                    date_formatted = datetime.datetime.strptime(date_str, "%Y-%m-%d").strftime("%b %d")
+                except ValueError as e:
+                    date_formatted = "Invalid date format"
+                    app.logger.error(f"Error parsing date for entry: {entry['_id']}. Error: {str(e)}")
+            else:
+                date_formatted = "No date"
+            
+            entries_with_date.append((content, date_str, date_formatted))
+        
+        return render_template("home.html", entries=entries_with_date)
+    
+    return app
+
